c1d123881bdabfc450d605b5c59548ccda5606dc
Lian Aga Aditya <admin@panicscript.com>
2014-12-12 14:49:26 +0700

    Image Upload Controller
    This Controller used to handle image upload which integrated with
    Redactor

---
+++ Redactor.php
@@ -0,0 +1,29 @@
+<?php 
+class Redactor extends CI_Controller {
+
+	public function image_upload()
+        {
+                if(!empty($_FILES['file']['name']))
+                {
+
+                        $config['upload_path'] = 'your_image_directories_relative_path';
+                        $config['allowed_types'] = 'gif|jpg|png';
+
+                        $this->load->library('upload', $config);
+
+                        if($this->upload->do_upload('file'))
+                        {
+                                $image_data = $this->upload->data();
+
+                                $json = array(
+                                    'filelink' => 'http://your.image.url/'.$image_data["file_name"],
+                                );
+
+                                echo stripslashes(json_encode($json));
+                        }
+                }
+        }
+     
+
+}
+?>
